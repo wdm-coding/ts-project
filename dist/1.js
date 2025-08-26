6dff4798b2797c561a2d0582c84c5f4d465221b6
weideming <wdmcoding0620@163.com>
2025-08-26 22:22:03 +0800

    2.13

--- dist/1.js
+++ dist/1.js
@@ -1,13 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let arr = [1, 2, 3];
-arr.push(4);
-console.log(arr);
-let obj = {
-    name: 'zhangsan',
-    age: 18,
-    sex: 'male'
-};
-obj.name = 'lisi';
-console.log(obj);
+var Week;
+(function (Week) {
+    Week[Week["Sun"] = 5] = "Sun";
+    Week[Week["Mon"] = 6] = "Mon";
+    Week[Week["Tue"] = 7] = "Tue";
+    Week[Week["Wed"] = 8] = "Wed";
+    Week[Week["Thu"] = 9] = "Thu";
+    Week[Week["Fri"] = 10] = "Fri";
+    Week[Week["Sat"] = 11] = "Sat";
+})(Week || (Week = {}));
+// console.log(Week.Sun)
+// console.log(Week['Sun'])
+// console.log(Week[12])
+var Config;
+(function (Config) {
+    Config["Debug"] = "debug";
+    Config["Production"] = "Production";
+    Config["Test"] = "Test";
+})(Config || (Config = {}));
+// console.log(Config.Debug)
+// console.log(Config['Debug'])
+// console.log(Config['Production'])
+var Month;
+(function (Month) {
+    Month[Month["Jan"] = 0] = "Jan";
+    Month[Month["Feb"] = 1] = "Feb";
+    Month[Month["Mar"] = 2] = "Mar";
+})(Month || (Month = {}));
+console.log(Month.Jan); // 0
+console.log(Month['Feb']); // 1
 //# sourceMappingURL=1.js.map
--- src/1.ts
+++ src/1.ts
@@ -1,11 +1,31 @@
-let arr:number[] = [1,2,3];
-arr.push(4);
-console.log(arr);
+enum Week {
+    Sun=5,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat
+}
 
-let obj = {
-    name: 'zhangsan',
-    age: 18,
-    sex: 'male'
-};
-obj.name = 'lisi';
-console.log(obj);
+// console.log(Week.Sun)
+// console.log(Week['Sun'])
+// console.log(Week[12])
+
+enum Config {
+    Debug = 'debug',
+    Production = 'Production',
+    Test = 'Test'
+}
+
+// console.log(Config.Debug)
+// console.log(Config['Debug'])
+// console.log(Config['Production'])
+
+const enum Month {
+    Jan,
+    Feb,
+    Mar,
+}
+console.log(Month.Jan) // 0
+console.log(Month['Feb']) // 1
