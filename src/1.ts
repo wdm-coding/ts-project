b4012ef98fa710b6e9c4001a91a80384e18e655a
weideming <wdmcoding0620@163.com>
2025-08-28 21:40:46 +0800

    微调

--- src/1.ts
+++ src/1.ts
@@ -1,4 +1,23 @@
-function fun(data:string,...rest:number[]){
-  console.log(data,rest); // 'hello',[1, 2, 3]
+class People {
+  name: string;
+  age?: number | undefined;
+  static count = 0;
+  constructor(name: string, age?: number) {
+    this.name = name;
+    this.age = age;
+    People.count++;
+  }
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+  static getCount() {
+    console.log(People.count);
+  }
 }
-fun('hello',1,2,3)
+
+let xh = new People('xh', 20);
+let yl = new People('yl', 20);
+xh.sayHello(); // 输出: Hello, my name is xh and I am 20 years old.
+People.getCount(); // 输出: 2
+
+3-9
